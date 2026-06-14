a92f17811d8686524123950c16ab135ca8caf6eb
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-14 23:30:55 +0530

    middle triangle pattern

--- star_patterns.py
+++ star_patterns.py
@@ -6,8 +6,10 @@
 #     print()
 
 n = int(input("Enter the number: "))
-
+j=0
 for i in range(0,n):
-    for j in range(0,i-j):
-        print("* ", end=" ");
+    for k in range(0,n-i-1):
+        print(" " , end="")
+    for j in range(0, 2 * i + 1):
+        print("*", end=" ");
     print()
