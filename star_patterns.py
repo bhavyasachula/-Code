74730e70db3e972ca738ce449d6d2710eb9cf914
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-14 23:24:42 +0530

    Triangle pattern

--- star_patterns.py
+++ star_patterns.py
@@ -1,6 +1,13 @@
+# n = int(input("Enter the number: "))
+
+# for i in range(0,n):
+#     for j in range(0,n):
+#         print("*", end=" ");
+#     print()
+
 n = int(input("Enter the number: "))
 
-for i in range(0,n-1):
-    for j in range(0,n-1):
+for i in range(0,n):
+    for j in range(0,i):
         print("*", end=" ");
     print()
