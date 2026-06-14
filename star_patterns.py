2d6bac0669b016b8acbfc86b787ea75a0eb6a749
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-14 23:38:20 +0530

    corrected middle version

--- star_patterns.py
+++ star_patterns.py
@@ -5,11 +5,21 @@
 #         print("*", end=" ");
 #     print()
 
+# n = int(input("Enter the number: "))
+# j=0
+# for i in range(0,n):
+#     for k in range(0,n-i-1):
+#         print("  " , end="")
+#     for j in range(0, 2 * i + 1):
+#         print("*", end=" ");
+#     print()
+
+
 n = int(input("Enter the number: "))
 j=0
 for i in range(0,n):
-    for k in range(0,n-i-1):
+    for k in range(i):
         print("  " , end="")
-    for j in range(0, 2 * i + 1):
+    for j in range(0, 2 * (n-i) + 1):
         print("*", end=" ");
     print()
