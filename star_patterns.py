14ce2831bd42178c114afd50608a1bdbb92b5c8d
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-14 23:39:31 +0530

    corrected inverted centered star pattern

--- star_patterns.py
+++ star_patterns.py
@@ -20,6 +20,6 @@ j=0
 for i in range(0,n):
     for k in range(i):
         print("  ", end="")
-    for j in range(0, 2 * (n-i) + 1):
+    for j in range(0, 2 * (n-i) - 1):
         print("*", end=" ");
     print()
