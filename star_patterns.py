f07320fe87f8159469b0df3eac1f6195dbce2cff
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-15 23:46:08 +0530

    Character D star pattern variation

--- star_patterns.py
+++ star_patterns.py
@@ -41,8 +41,8 @@ rows = 5
 for i in range(rows):  # Loop for rows
     for j in range(rows):  # Loop for columns
         if i == 0 or i == rows - 1 or j == 0 or j == rows - 1:  
-            print("*", end="")  # Print stars at borders
+            print("*", end="  ")  # Print stars at borders
         else:  
-            print("", end="")  # Print spaces inside
+            print("  ", end="  ")  # Print spaces inside
     print()
 
