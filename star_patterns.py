ba0ee81e92a38d5e45aa2f12370a04b34f1dfe1d
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-15 23:41:35 +0530

    Inverted 7 star pattern

--- star_patterns.py
+++ star_patterns.py
@@ -33,5 +33,5 @@ for i in range(1, rows + 1):
         if k == 0 or k == 2 * i - 2 or i == rows:  # Print stars at borders
             print("*", end="")  
         else:  
-            print(" ", end="")  # Print spaces inside
+            print("", end="")  # Print spaces inside
     print()
