701e7984f283e89cf6e2cfd04a1071c2d37a0e25
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-15 23:38:55 +0530

    hollow pyramid star pattern

--- star_patterns.py
+++ star_patterns.py
@@ -23,3 +23,15 @@
 #     for j in range(0, 2 * (n-i) - 1):
 #         print("*", end=" ");
 #     print()
+
+rows = 5  
+
+for i in range(1, rows + 1):  
+    for j in range(rows - i):  # Print spaces
+        print(" ", end=" ")  
+    for k in range(2 * i - 1):  
+        if k == 0 or k == 2 * i - 2 or i == rows:  # Print stars at borders
+            print("*", end=" ")  
+        else:  
+            print(" ", end=" ")  # Print spaces inside
+    print()
