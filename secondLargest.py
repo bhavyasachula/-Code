ffdfc2949f358e3acd77722a745d0f6002d1df17
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-16 23:03:40 +0530

    second largest element

---
+++ secondLargest.py
@@ -0,0 +1,10 @@
+a = [1,4,7,7,5]
+largest = 0
+secondLargest = 0 
+for i in a:
+    if(i>largest):
+        largest = i
+for i in a:
+    if(i>secondLargest and i!=largest):
+        secondLargest = i
+print("Second largest Element in Array :",secondLargest) 
