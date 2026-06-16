d40e6a131f9f7a69a6052d9db216bde915a5d872
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-16 22:09:15 +0530

    revised the logic to know more about how it works

--- HighestOccurenceChar.py
+++ HighestOccurenceChar.py
@@ -1,12 +1,17 @@
-string = input("Enter the string :")
+# string = input("Enter the string :")
 
 count = {}
 
-for s in string:
-   count[s] = count.get(s,0) + 1
-   if(count[s]>1):
-      print(s,":",count[s])
-    
-    
-
+# for s in string:
+#    count[s] = count.get(s,0) + 1
+   
+# print(count)
+a = "bhavya"
+for i in a:
+    count[i] = count.get(i,0) + 1
+   
+for k in count:
+    if count[k] > 1:
+      print(k)
+   
 
