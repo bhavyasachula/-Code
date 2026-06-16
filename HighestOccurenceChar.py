9f9e4fc6f5c2dd35b7588e2256440ad29cbbc9ae
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-16 22:10:01 +0530

    unnessary cleanup

--- HighestOccurenceChar.py
+++ HighestOccurenceChar.py
@@ -1,11 +1,5 @@
-# string = input("Enter the string :")
-
 count = {}
 
-# for s in string:
-#    count[s] = count.get(s,0) + 1
-   
-# print(count)
 a = "bhavya"
 for i in a:
     count[i] = count.get(i,0) + 1
