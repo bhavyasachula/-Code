e3b0850323b215bbc9b4c504f0a887d8342c9ec9
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-16 22:33:31 +0530

    finding largest from the array

--- HighestOccurenceChar.py
+++ HighestOccurenceChar.py
@@ -1,11 +1,18 @@
-count = {}
+# count = {}
 
-a = "bhavya"
+# a = "bhavya"
+# for i in a:
+#     count[i] = count.get(i,0) + 1
+   
+# for k in count:
+#     if count[k] > 1:
+#       print(k)
+   
+
+a = [1,4,5,6,2]
+largest = 0
 for i in a:
-    count[i] = count.get(i,0) + 1
-   
-for k in count:
-    if count[k] > 1:
-      print(k)
-   
+    if(a[i]>largest):
+        largest = a[i]
 
+print(largest)
