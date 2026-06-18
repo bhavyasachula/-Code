ff59fd9ce5a3e9ea5b51e02e32429fa1f825271a
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-18 15:05:53 +0530

    Optimal unqiue elem procedure

--- optimalusUnique.cpp
+++ optimalusUnique.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 vector<int> arr = {1,1,1,1,2,2,2,2,2,4,4,4,4};
- 
+// two pointers approach i pointer will be at index 0  and j pointer will be at index 1 then j pointer traverse the array and checks is the element of arr[i] is not equall then place it beside the ith index and point to it / i++ and return the index+1 of i to know how many unique elements are there;
 int optimalUniqueElem(vector<int> arr){
     int i=0;
     for(int j=1; j<arr.size(); j++){
