69df0135aabe19e50c7ddf7f6f0e9c32654d13db
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-18 14:52:02 +0530

    line edits

--- isSorted.cpp
+++ isSorted.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 int isSortedAsc(vector<int> arr){
     for(int i=1;i<arr.size();i++){
-        if(arr[i]>=arr[i-1]){
-            
+        if(arr[i]>=arr[i-1]){   
         }
         else{
             return 0;
